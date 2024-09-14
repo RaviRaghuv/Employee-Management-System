719fd5dbecc26d559422a29168110fc46365eeb7
RaviRaghuv <ravikumarswn5@gmail.com>
2024-09-15 02:19:46 +0530

    first commit

---
+++ employeemanagement-frontend/src/component/FooterComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FooterComponent = () => {
+    return (
+        <div>
+            <footer className='footer'>
+            <span>All Rights Reserved &copy; {new Date().getFullYear()} | Developed by Ravi</span>
+
+            </footer>
+        </div>
+    )
+}
+
+export default FooterComponent
